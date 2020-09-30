bdb0f8bb9d731f2d503ea9299f538544dcd27256
Dominik Kesim <Dominik.Kesim@gmail.com>
2020-09-30 14:59:00 +0200

    working authentication with google+ api

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const path = require('path')
 const connectDB = require('./config/db')
+const passport = require('passport')
+const session = require('express-session')
 
 // dotenv is a module that loads environment variables from
 // a .env file into the environment
 // based on Twelve-Factor-App-Methodology
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -24,11 +29,23 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 // whenever we use 'process.env' we can use global variables
 const PORT = process.env.PORT || 5000
