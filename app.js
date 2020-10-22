4cf3ff26d5eea7f54be37f14499083c9dedc5866
Dominik Kesim <Dominik.Kesim@gmail.com>
2020-10-22 18:09:15 +0200

    added buttons and new route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const path = require('path')
 const session = require('express-session')
 const mongoose = require('mongoose')
@@ -23,17 +24,39 @@ connectDB()
 const app = express()
 
 // Body parser
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method
+            delete req.body._method
+            return method
+        }
+    })
+)
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({
+    helpers: {
+        formatDate,
+        truncate,
+        stripTags,
+        editIcon,
+        select
+    }, defaultLayout: 'main', extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -44,9 +67,16 @@ app.use(session({
     store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
+
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
